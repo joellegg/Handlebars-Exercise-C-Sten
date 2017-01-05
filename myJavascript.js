db8f375a9ec243451ea3aa48d249d365bc48cccf
joellegg <joellegg@bellsouth.net>
2017-01-05 13:21:24 -0600

    ajax call and promise

--- myJavascript.js
+++ myJavascript.js
@@ -23,16 +23,32 @@ var randomUser = {
   ]
 }
 
-//handlebars step one: grab the html from the script tag
-let entryHTML = $('#handlingScript').html();
+function loadUser(data) {
+  //handlebars step one: grab the html from the script tag
+  let entryHTML = $('#handlingScript').html();
 
-//handlebars step two: compile it into a template
-let entryTemplate = Handlebars.compile(entryHTML)
+  //handlebars step two: compile it into a template
+  let entryTemplate = Handlebars.compile(entryHTML)
 
-//handlebars step three:render the HTML by passing the data to the template
-let entryOutput = entryTemplate(randomUser);
+  //handlebars step three:render the HTML by passing the data to the template
+  let entryOutput = entryTemplate(data);
 
-console.log(entryOutput);
+  //handlebars step four: put the complete HTML into the DOM
+  $('#theOutput').append(entryOutput);
+}
 
-//handlebars step four: put the complete HTML into the DOM
-$('#theOutput').append(entryOutput);
+//ajax call to get random user
+function getUser() {
+  return new Promise(function(resolve, reject) {
+    $.ajax({
+      url: 'https://randomuser.me/api/',
+      dataType: 'json'
+    }).done(function(data){
+      resolve(data)
+    })
+  })
+}
+
+getUser().then(function(data){
+  return loadUser(data);
+})
